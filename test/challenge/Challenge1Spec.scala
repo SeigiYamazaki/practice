b187f1ff2046168042174fc5842e41cded74c9b1
山崎正義 <yamazaki.seigi@donuts.ne.jp>
2019-10-03 16:45:49 +0900

    challenge1作成

--- test/challenge/Challenge1Spec.scala
+++ test/challenge/Challenge1Spec.scala
@@ -7,6 +7,13 @@ class Challenge1Spec extends WordSpec {
   val challenge = new Challenge1
 
   "challenge1" should {
-    "" is pending
+    "フィボナッチ数列からn番目の数を取得する再帰関数を記述してください。" in {
+      assert(challenge.fib(0) == 0)
+      assert(challenge.fib(1) == 1)
+      assert(challenge.fib(3) == 2)
+      assert(challenge.fib(4) == 3)
+      assert(challenge.fib(7) == 13)
+      assert(challenge.fib(46) == 1836311903)
+    }
   }
 }
